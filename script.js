577d1947ae152d17249b342721daf89443360168
Ayako-a-due30 <sonnige.seite.str@gmail.com>
2023-06-20 19:47:54 +0900

    家具販売サイト

---
+++ script.js
@@ -0,0 +1,24 @@
+window.addEventListener("DOMContentLoaded", ()=> {
+// ----トグルボタン----------------------
+const toggleButton = document.querySelector(".toggle-button");
+const toggleMenu = document.querySelector(".toggle-menu");
+
+toggleButton.addEventListener("click", ()=>{
+    toggleMenu.classList.toggle("show-menu");
+});
+
+
+
+// -------商品一覧(トップページ)-----------
+const productWrap = document.querySelector(".product");
+
+    for (let i = 1; i < 9; i++) {
+      const content = `
+        <div class="product-item">
+          <img src="./img/item${i}.jpg" alt="" class="product-img">
+          <dt>プロダクト</dt>
+          <dd>¥99,999 +tax</dd>
+        </div>`;
+      productWrap.insertAdjacentHTML("afterbegin", content);
+    }
+});
