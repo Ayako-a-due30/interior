8b4cc9fe2bfa0e6f2683581012446d45a59b6a12
Ayako-a-due30 <sonnige.seite.str@gmail.com>
2023-06-21 16:01:19 +0900

    ざっくり全体を作成

--- script.js
+++ script.js
@@ -2,25 +2,57 @@ window.addEventListener("DOMContentLoaded", ()=> {
 // ----トグルボタン----------------------
 const toggleButton = document.querySelector(".toggle-button");
 const toggleMenu = document.querySelector(".toggle-menu");
+const toggleItem = document.querySelector(".toggle-item");
 
 toggleButton.addEventListener("click", ()=>{
     toggleMenu.classList.toggle("show-menu");
+    toggleButton.classList.toggle("show-menu");
+    toggleItem.animate(
+      {
+        translate:['-4rem',0],
+      },
+      {
+        duration:300,
+      }
+    );
 });
 
 
+});
 
-// -------商品一覧(トップページ)-----------
+// -------商品一覧(index.html)-----------
+window.addEventListener("DOMContentLoaded", ()=> {
+
 const productWrap = document.querySelector(".product");
 
-    for (let i = 1; i < 9; i++) {
+    for (let i = 8; i > 0; i--) {
       const content = `
         <div class="product-item">
-        <a href="#">
+        <a href="./item${i}.html">
         <img src="./img/item${i}.jpg" alt="" class="product-img">
         <dt>プロダクト</dt>
         <dd>¥99,999 +tax</dd>
         </a>
         </div>`;
       productWrap.insertAdjacentHTML("afterbegin", content);
+    }
+});
+
+// -------商品一覧(product.html)-----------
+window.addEventListener("DOMContentLoaded", ()=> {
+
+const allProductWrap = document.querySelector(".all-product");
+
+    for (let i = 16; i > 0; i--) {
+      const content = `
+        <div class="product-item">
+        <a href="./item${i}.html">
+        
+        <img src="./img/item${i}.jpg" alt="" class="product-img">
+        <dt>プロダクト</dt>
+        <dd>¥99,999 +tax</dd>
+        </a>
+        </div>`;
+      allProductWrap.insertAdjacentHTML("afterbegin", content);
     }
 });
